75fee2548a5eec78e7723e11dced2eed8a9f1c1a
NewbyJE <NewbyJE@comcast.net>
2014-02-23 12:38:42 -0800

    BoardConfig.mk: No longer using lib_driver_cmd_ath6kl

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,13 +46,13 @@ BOARD_WLAN_DEVICE := ath6kl
 # ATH6KL uses NL80211 driver
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
+# BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
 # Station/client mode
 WIFI_DRIVER_MODULE_NAME := "ath6kl"
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ath6kl.ko"
 # ATH6KL uses hostapd built from source
 BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
+# BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
 # FW path parameters (todo: temp, should be ignored)
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
