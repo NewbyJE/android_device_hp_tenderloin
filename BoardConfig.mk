05c231d860bec8cf4af4ee69d015966df26786cd
NewbyJE <NewbyJE@comcast.net>
2014-09-13 10:08:04 -0700

    misc corrections

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,11 +33,17 @@ WIFI_DRIVER_LOADER_DELAY := 1000000
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/ath6kl.ko"
-WIFI_DRIVER_MODULE_NAME     := "ath6kl"
+# Station/client mode
+WIFI_DRIVER_MODULE_NAME := "ath6kl"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ath6kl.ko"
 # ATH6KL uses hostapd built from source
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
+# FW path parameters (todo: temp, should be ignored)
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP  := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_DRIVER_FW_PATH_PARAM := ""
 
 # Audio
 BOARD_USES_LEGACY_ALSA_AUDIO := true
